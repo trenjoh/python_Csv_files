25bf802461a6809d27cd75f8adea3c26019834ee
trenjoh <156889109+trenjoh@users.noreply.github.com>
2024-04-05 02:15:47 +0300

    Update reading_csv.py

--- reading_csv.py
+++ reading_csv.py
@@ -1,12 +1,12 @@
-# importing dat using dict reader
-# from csv import reader
-# with open("1.1 fighters.csv.csv") as file:
-#     csv_reader = reader(file )
-#     # for row in csv_reader:
-#     #     print(row)
-#     next(csv_reader) # if you don't the header
-#     for fighter in csv_reader:
-#         print(f"{fighter[0]} is from {fighter [1]} adress {fighter[2]}")
+#importing  using  reader
+from csv import reader
+with open("1.1 fighters.csv.csv") as file:
+    csv_reader = reader(file )
+    # for row in csv_reader:
+    #     print(row)
+    next(csv_reader) # if you don't the header
+    for fighter in csv_reader:
+        print(f"{fighter[0]} is from {fighter [1]} adress {fighter[2]}")
 
 # using dict reader
 # has ordered dictionaries
